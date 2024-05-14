c75ec035566f4109c568f911ad172c70a3241892
Aakarshak Sthapak <akku_sthapak@live.com>
2024-05-14 23:54:28 +0530

    Added Board and Menu Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { Provider } from 'react-redux';
 import store from '@/src/store';
 import './App.css';
 
+import Board from '@/src/components/Board';
+import Menu from '@/src/components/Menu';
+
 const App = () => {
   return (
     <Provider store={store}>
-      <div className='App'>Hello</div>;
+      <>
+        <Board />
+        <Menu />
+      </>
     </Provider>
   );
 };
